7bfb45a4b1202f2906820b2f00c4d0f89b836fb2
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-09-30 17:06:17 +0000

    DivisizioneDi2Fattori

---
+++ DivisizioneDi2Fattori.c
@@ -0,0 +1,33 @@
+/*Verificare se un numero dato in input è
+divisibile sia per 3 sia per 5*/
+
+#include <stdio.h>
+int main (){
+    int numero=0;
+
+    printf("inserisci un numero a piacere");
+    scanf("%d", &numero);
+
+    if(numero ){
+        printf(il numero%d è divisibile sia per tre sia per cinque);
+    }
+    else{
+        if(numero /5)
+            printf("il numero %d è divisibile per cinque");
+        else
+            printf("il numero %d non è divisibile ne per cinque ne per tre");    
+    }
+    else{
+        if(numero /3)
+            printf("il numero %d è divisibile per tre");
+        else
+            printf("il numero %d non è divisibile ne per tre ne per cinque");
+    }
+    return 0;
+}
+
+
+        
+
+
+    
---
+++ Piccolo_Grande.c
@@ -0,0 +1,20 @@
+/*Dati 3 numeri stampare il più piccolo, il più grande
+ e la loro media aritmetica */
+
+ #include <stdio.h>
+ int main(){
+    int a=0;
+    int b=0;
+    int c=0;
+
+    printf("inserisci il primo valore: ");
+    scanf("%d", &a);
+    printf("inserisci il secondo valore: ");
+    scanf("%d", &b);
+    printf("inserisci il terzo valore: ");
+    scanf("%d", %c);
+
+    
+    
+
+ }
