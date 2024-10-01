89ada8121cc1a82ac35c6f1a45dab5248631ff6c
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-10-01 07:37:06 +0000

    DivisizioneDi2Fattori

--- DivisizioneDi2Fattori.c
+++ DivisizioneDi2Fattori.c
@@ -8,23 +8,23 @@ int main (){
     printf("inserisci un numero a piacere");
     scanf("%d", &numero);
 
-    if(numero /5) and (numero /3){
-        printf("il numero%d è divisibile sia per tre sia per cinque");
+    if(numero% 5 == 0){
+        if(numero% 3 == 0){
+            printf("%d è divisibile sia per 5 che per 3", numero);
+        }
+        else{
+            printf("%d è divisibile per 5 ma non per 3", numero);
+        }
     }
     else{
-        if(numero /5)
-            printf("il numero%d è divisibile per cinque");
-        else
-            printf("il numero%d non è divisibile ne per cinque ne per tre");    
-    }
-    else{
-        if(numero /3)
-            printf("il numero%d è divisibile per tre");
-        else
-            printf("il numero%d non è divisibile ne per tre ne per cinque");
+        printf("%d non è divisibile per 5", numero);
     }
     return 0;
 }
+
+
+    
+
 
 
         
