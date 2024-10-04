9b3fcd56aed6d7cfbf3f8ce97ac32f45d1bead91
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-10-04 13:59:38 +0000

    TriagoloEquilatero

--- informatica/TriagoloEquilatero.c
+++ informatica/TriagoloEquilatero.c
@@ -6,4 +6,29 @@ int main(){
     float lato1=0;
     float lato2=0;
     float lato3=0;
-   
+    float area=0;
+    float perimetro=0;
+
+    printf("inserisci il primo lato: ");
+    scanf("%f", &l1);
+    printf("inserisci il secondo lato: ");
+    scanf("%f", &l2);
+    printf("inserisci il terzo lato: ");
+    scanf("%f", &l3);
+
+    if(l1==l2){
+        if(l2==l3){
+            printf("il triangolo è equilatero: ");
+            perimetro= l1+l2+l3;
+            area= sqrt(3)*l1/2*l1/2;
+            printf("area è %f, il perimetro è %f", area, perimetro);
+        }
+        else{
+             printf("il triangolo non è equilatero: ");
+        }
+    }
+    else{
+        printf("il triangolo non è equilatero: "); 
+    }
+    return 0;
+}
