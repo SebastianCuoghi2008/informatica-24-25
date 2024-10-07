57fb4f78f7a7c62bd033546fb5f9a8f0e0848cd2
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-10-07 11:42:11 +0000

    SommaCifre

---
+++ informatica/SommaCifre.c
@@ -0,0 +1,17 @@
+/*dato un numero a più cifre
+eseguire la somma delle cifre e stabilire
+se è dispari per 3*/
+
+#include<stdio.h>
+
+int main(){
+    int numero=0;
+    int cifra=0;
+    int somma=0;
+    int quoziente=0;
+    int resto=0;
+
+    printf("inserisci un numeo: ");
+    scanf("%d", &numero);
+
+    
