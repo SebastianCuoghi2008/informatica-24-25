6860bcb1587666647ae0c05c674f4db65e75c07b
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-10-18 17:52:22 +0000

    LaVocale

--- informatica/LaVocale.c
+++ informatica/LaVocale.c
@@ -1,16 +1,19 @@
-/*INSERITO UN CARATTERE DIRE SE E' UNA VOCALE*/
+/*INSERITO UN carattereATTERE DIRE SE E' UNA VOCALE*/
 
 #include <stdio.h>
 
 int main(){
-	char car;
+	char carattere;
 	
 	printf("Inserisci un carattere: ");
-	scanf("%c", &car);
-	printf("Il carattere inserito vale: %c", car );
+	scanf("%c", &carattere);
+	printf("Il carattere inserito vale: %c", carattere );
 	
-	if(car=='a' || car=='e' || car=='i' || car=='o' || car=='u')
-		printf("\nIl carattere %c è una vocale.", car);
-	else
-		printf("\nIl carattere %c non è una vocale.", car);
+	if(carattere=='a' || carattere=='e' || carattere=='i' || carattere=='o' || carattere=='u' || carattere=='A' || carattere=='E' || carattere=='I' || carattere=='O' || carattere=='U'){
+		printf("\nIl carattere %c è una vocale.", carattere);
+	}
+	else{
+		printf("\nIl carattere %c non è una vocale.", carattere);
+	}
+	return 0;
 }
