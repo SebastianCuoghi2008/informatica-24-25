2864530d3ea8e6983fb6fa4b6c5bb9171e4bc8d9
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-10-24 13:00:03 +0000

    LaCalcolatrice

---
+++ informatica/LaCalcolatrice.c
@@ -0,0 +1,33 @@
+#include <stdio.h> 
+int main(){
+    int numero1=0;
+    int numumo2=0;
+    char operazione;
+
+    printf("quale operazione vuoi fare? ");
+    scanf("%c", &operazione);
+    switch(operazione){
+        case 'A':
+            A=numero1+numero2;
+            printf("il risultato é: ");
+            berak;          
+        case 'S':
+            S=numero1-numero2;
+            printf("il risultato é: ");
+            berak;          
+        case 'M':
+            M=numero1*numero2;
+            printf("il risultato é: ");
+            berak;          
+        case 'D':
+            D=numero1/numero2;
+            printf("il risultato é: ");
+            berak;
+        default:
+            printf(il numero inserito non è valido);
+    }
+    return 0;
+}      
+
+
+        
