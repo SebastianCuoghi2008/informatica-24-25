bf435afe52345be12c2cedeaa0e1518389f65081
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-11-03 13:19:32 +0000

    EuroODollaro

--- informatica/EuroODollaro.c
+++ informatica/EuroODollaro.c
@@ -10,9 +10,29 @@ una somma di denaro calcolare il controvalore.*/
 
 int main(){
     float sommaDenaro=0;
-    float dollaro=1.23;
-    float euro=0,82;
+    float Dollaro=1.23;
+    float Euro=0,82;
     int scelta;
 
-    printf(inserisci una somma di denaro)
+    printf("inserisci una somma di denaro: ");
+    scanf("%f", &sommaDenaro);
+    printf("scegli tra Dollaro (1) o Euro (2), per come vuoi trasformare la somma di denaro inserita: ");
+    scanf("%d", &scelta);
+
+    switch (scelta){
+        case 1:
+            printf("Euro a Dollaro");
+            sommaDenaro=sommaDenaro*Dollaro;
+            printf("lo scampio in Dollaro vale %f, sommaDenaro");
+            break;
+        case 2:
+            printf("Dollaro a Euro");
+            sommaDenaro=sommaDenaro*Euro;
+            printf("lo scampio in Euro vale %f, sommaDenaro");
+            break;
+        default:
+            printf("non inserire lettere o caratteri che non c'entrano");
+            break;       
+    }
+    return 0;
 }
