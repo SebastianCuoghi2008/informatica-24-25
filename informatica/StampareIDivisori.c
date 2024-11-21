31ef57c119ef0b2b0c710293453cfb55450335c9
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-11-21 10:51:03 +0000

    StampareIDivisori

---
+++ informatica/StampareIDivisori.c
@@ -0,0 +1,19 @@
+/*Dato un numero stampare tutto i suoi divisori*/
+
+#include <stdio.h>
+int main(){
+    int numero=0;
+    int divisore=2;
+
+    printf("inserisci un numero: ");
+    scanf("%d", &numero);
+ 
+    while(divisore<=numero/2){
+        if(numero%divisore==0){
+            printf("%d è un divisore di %d\t", divisore, numero);
+        }
+        divisore++;           
+    }
+    printf("1\t%d", numero);   
+    return 0;
+}
