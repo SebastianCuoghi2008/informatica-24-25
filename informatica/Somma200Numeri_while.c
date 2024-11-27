273e5cb82fcb5b293c7794b61f76e195c686bbb0
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-11-27 08:35:49 +0000

    Somma200Numeri_while

--- informatica/Somma200Numeri_while.c
+++ informatica/Somma200Numeri_while.c
@@ -4,15 +4,15 @@
 #include <stdio.h>
 
 int main(){
-    float num=0, somma = 0;
+    float numero=0, somma = 0;
     float media = 0;
     int conta = 0;
 
     while(somma<200 && conta<10){
-        printf("Inserisci il primo numero:");
-        scanf("%f", &num);
-        conta = conta + 1; //conta++;   conta+=1;
-        somma = somma + num;
+        printf("Inserisci il primo numero: ");
+        scanf("%f", &numero);
+        conta = conta + 1; //conta++; conta+=1;
+        somma = somma + numero;
     }
     media = somma / conta;
     printf("\nla media è %.2f, il numero di tentativi è %d", media, conta);
