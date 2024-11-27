3635499135250075376645ffd314c4a486c05c9e
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-11-27 08:49:48 +0000

    OgniNumeroCompreso_for

---
+++ informatica/OgniNumeroCompreso_for.c
@@ -0,0 +1,15 @@
+/*Scrivi un programma che chieda un numero intero all'utente e mostri tutti i numeri compresi tra il numero scelto e 0.*/
+
+#include <stdio.h>
+int main(){
+    int num;
+    do
+    {
+        printf("Inserisci un valore: ");
+        scanf("%d", &num);
+    } while (num<0);
+    
+    for(int i=0; i<num; i++){
+        printf("%d\n", i);
+    }
+}
---
+++ informatica/OgniNumeroCompreso_for_parte2.c
@@ -0,0 +1,17 @@
+/*Scrivi un programma che chieda un numero intero all'utente e mostri tutti i numeri compresi tra il numero scelto e 0.*/
+
+#include <stdio.h>
+int main(){
+    int num;
+
+    do
+    {
+        printf("Inserisci un valore: ");
+        scanf("%d", &num);
+    } while (num<0);
+    
+    for(int i=num; i>=0; i--){
+        printf("%d\n", i);
+    }
+    return 0;
+}
