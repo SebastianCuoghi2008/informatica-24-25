25b97f891b52915518991054db6039542d893ecd
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-12-02 10:51:11 +0000

    SommeSuccessive

---
+++ informatica/IPrimi100Numeri.c
@@ -0,0 +1,1 @@
+/*Scrivi un programma che visualizzi i primi 100 numeri dispari a gruppi di 5.*/
--- informatica/SommeSuccessive.c
+++ informatica/SommeSuccessive.c
@@ -3,20 +3,25 @@ delle somme successive dopo aver controllato l’input e accettato solo valori m
 
 #include <stdio.h>
 int main(){
-    int numero=0;
-    int prodotto;
+    int numero1=0;
+    int numero2=0;
+    int somma=0;    //le variabili sommatorie e contatore vanno inizializzate a 05
+    int i=0;
 
     do
     {
-        printf("Inserisci un valore: ");
-        scanf("%d", &numero);
-    } while (numero<0);
-    prodotto=numero;
-
-    for(int i=1; i<=11; i++){ 
-
-        prodotto=prodotto+numero;
-        printf("%d\n", prodotto);
+        printf("Inserisci il primo valore: ");
+        scanf("%d", &numero1);
+    }while (numero1<0);
+     do
+    {
+        printf("Inserisci il secondo valore: ");
+        scanf("%d", &numero2);
+    }while (numero2<0);
+    
+    for(int i=0; i<numero1; i++){ 
+        somma+=numero2;     //somma=somma+numero2       
     }
+    printf("il prodotto tra %d e %d vale: %d", numero1, numero2, somma);
     return 0;
 }
