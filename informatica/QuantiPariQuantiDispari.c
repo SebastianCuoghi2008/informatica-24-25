12fcd2fa177d33eacb2e0199c94c3a321d2236c6
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2024-12-14 16:08:07 +0000

    QuantiPariQuantiDispari

--- informatica/QuantiPariQuantiDispari.c
+++ informatica/QuantiPariQuantiDispari.c
@@ -5,20 +5,20 @@ Determinare quanti sono stati i valori pari e i valori dispari.*/
 
 int main(){
 	int numero=0;
-	int contatorePari=0;
-	int contatoreDispari=0;
+	int contatoreP=0;
+	int contatoreD=0;
 	
 	do{
 		printf("inserisci un numero: ");
 		scanf("%d", &numero);
 		if(numero%2==0 && numero!=0){	//lo 0 deve essere considerato come un numero pari
-			contatorePari++;
+			contatoreP++;
 		}
 		else{
-			contatoreDispari++;
+			contatoreD++;
 		}		
 	}while(numero!=0);
-	printf("il nuemero di valori pari inserito è %d", contatorePari);
-	printf("il nuemero di valori dispari inserito è %d", contatoreDispari);
+	printf("il nuemero di valori pari inserito è %d", contatoreP);
+	printf("il nuemero di valori dispari inserito è %d", contatoreD);
 	return 0;
 }
