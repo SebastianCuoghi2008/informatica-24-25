04567b031810c5b29d5e03982fe15a2cb32a14a6
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-01-20 10:50:47 +0000

    CuoghiEsQuarto16012025

--- informatica/CuoghiEsQuarto16012025.c
+++ informatica/CuoghiEsQuarto16012025.c
@@ -7,10 +7,30 @@ numero è uguale al secondo numero e viceversa.*/
 int main(){
     int numero1=0;
     int numero2=0;
+    int somma1=0;
+    int somma2=0;
+    int i;
 
-    printf("inserisci il primo numero: ");
-    scanf("%d," &numero1);
-    printf("inserisci il secondo numero: ");
-    scanf("%d," &numero2);
+    do{
+        printf("inserisci due valori: ");
+        scanf("%d %d", &numero1, &numero2);
+    }while(numero1<=0 || numero2<=0);
+
+    for(int i=1; i<numero1/2; i++){
+        if(numero1%i==0){
+            somma1+=i;
+        }
+    }
+    for(int i=1; i<numero2/2; i++){
+        if(numero2%i==0){
+            somma2+=i;
+        }
+    }
+    if(somma1==numero2 && somma2==numero1){
+        printf("\n i numeri sono amicali");
+    }
+    else{
+        printf("\n i numeri non sono amicali");
+    }
     return 0;
 }
