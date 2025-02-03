b7ebe2a7894085c346333ab7e1aba732f62df6c1
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-02-03 10:55:08 +0000

    ConversioneDecimaleABinario

---
+++ informatica/Funzioni/ConversioneDecimaleABinario.c
@@ -0,0 +1,31 @@
+/*dato un valore,eseguire la conversione da decimale a binario*/
+
+#include <stdio.h>
+#include <math.h>
+
+long converti(int _N);
+int main(){
+    int numero=0;
+    long convertito=0;
+
+    do{
+        printf("inserisci un numero: ");
+        scanf("%d", &numero);
+    }while(numero<=0);
+    convertito=converti(numero);
+    printf("%ld\n", convertito);
+}
+long converti(int _N){
+    int quoziente=_N;
+    int resto=0;
+    int contatore=0;
+    int _convertito=0;
+
+    while(quoziente!=0){
+        resto=quoziente%2;
+        quoziente=quoziente/2;
+        _convertito=pow(10, contatore);
+        contatore++;
+    }
+    return _convertito;
+}
