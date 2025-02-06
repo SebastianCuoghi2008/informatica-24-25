5effca250e5ca2ee2a060801f3ca0b7705162490
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-02-06 14:58:29 +0000

    Cerchio

--- informatica/Funzioni/Cerchio.c
+++ informatica/Funzioni/Cerchio.c
@@ -3,7 +3,30 @@ eseguire il passaggio dei parametri per indirizzo
 cerchio=2*PI*raggio  PI=3.14    Ac=PI*raggio+raggio*/
 
 #include <stdio.h>
+#include <stdlib.h>
+
+const float PI=3.14;
+float calcola_circonferenza(float *_R);
+float calcola_area(float *_R)
 
 int main(){
-    
+    float raggio;
+    float circonferenza;
+    float area;
+
+    do{
+    printf("inserisci un valore di raggio: ");
+    scanf("%f", &raggio);
+    }while(raggio<0);
+
+    area=calcola_area(*_R);
+    circonferenza=calcola_circonferenza(*_R);
+    printf("l'area è %f, la circonferenza è %f", area, circonferenza);
+    return 0;
+}
+float calcola_area(float *_R){
+    return *_R**_R*PI;
+}
+float calcola_circonferenza(float *_R){
+    return 2*PI**_R;
 }
