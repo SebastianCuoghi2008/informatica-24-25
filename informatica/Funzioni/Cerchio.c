949d62a69fb8040881161119576656f8ccae13fc
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-02-06 10:49:42 +0000

    Cerchio

---
+++ informatica/Funzioni/Cerchio.c
@@ -0,0 +1,9 @@
+/*dato il raggio calcolare la circonferenza e l'area del cerchio,
+eseguire il passaggio dei parametri per indirizzo
+cerchio=2*PI*raggio  PI=3.14    Ac=PI*raggio+raggio*/
+
+#include <stdio.h>
+
+int main(){
+    
+}
