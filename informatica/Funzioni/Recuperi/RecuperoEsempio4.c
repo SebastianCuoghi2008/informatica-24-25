f5a83246d1be9ef745b311dbc02428c4e257ee86
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-02-12 13:46:47 +0000

    RecuperoEsempio4

---
+++ informatica/Funzioni/Recuperi/RecuperoEsempio4.c
@@ -0,0 +1,31 @@
+/*Scrivi un programma in C che chieda il numero di studenti di una classe, quindi per ogni studente viene inserito il voto di 4 materie. Il programma deve mostrare la media per ogni studente.*/
+
+#include <stdio.h>
+
+int main(){
+    float media;
+    float somma_voti=0;
+    float voto;
+    const int materie=4;
+    int studenti;
+
+    do{
+        printf("Inserisci numero studenti: ");
+        scanf("%d", &studenti);
+    }while(studenti<=0);
+    
+    for(int j=1; j<=studenti; j++){
+        printf("Studente %d\n", j);
+        somma_voti = 0;
+        for(int i=1; i<=materie; i++){
+            do{
+                printf("Inserisci voto materia %d", i);
+                scanf("%f", &voto);
+            }while(voto<2 || voto>10);
+            somma_voti+=voto;
+        }
+        media = somma_voti / materie;
+        printf("La media dello studente %d è %.1f\n", j, media);
+    }
+    
+}
