2a05b0ef4fe79703135ee4a098ae0194c8b4f21f
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-02-17 10:55:49 +0000

    NumeriAmicali

---
+++ informatica/Funzioni/NumeriAmicali.c
@@ -0,0 +1,18 @@
+/*data una sequenza di valori, verificare se i valori a coppie si terminano quando uno dei valori è 0*/
+
+#include <stdio.h>
+
+int amicali(int *_N1, *_N2);
+
+int main(){
+    int N1=0;
+    int N2=0;
+
+    do{
+        printf("inserisci due valori: ");
+        scanf("%d%d", N1, N2);
+    }while(N1==0 && N==2);
+}
+   
+
+   
