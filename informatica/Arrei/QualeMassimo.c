62a5de667e7e444f2b11dad11e6d0dc6de1c5ff7
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-03-19 13:52:53 +0000

    QualeMassimo

---
+++ informatica/Arrei/QualeMassimo.c
@@ -0,0 +1,28 @@
+/*Scrivi un programma in C che riempia un vettore di 10 elementi con numeri casuali compresi
+tra 0 e 100. Quindi fai in modo che il programma possa mostrare i due valori maggiori presenti
+nel vettore. Suddividi il programma in funzioni riempiVettore(), stampaVettore(), stampaDueMax().*/
+
+#include <stdio.h>
+#include "libArray.h"
+#include "libArray.c"
+
+#define DIM 10
+void stampaDueMax(int vettore[], int dim);
+int main(){
+    int vettore[DIM];
+    riempiVettoreCasuale(vettore, DIM, 0, 100);
+    stampaVettore(vettore, DIM, '.');
+}
+void stampaDueMax(int vettore[], int dim){
+    int max1=vettore[0];
+    int max2=vettore[0];
+    for(int i=1; i<dim; i++){
+        if(max1<vettore[i]){
+            max2=max1;
+            max1=vettore[i];
+        }else if(vettore[i]>max2 && vettore[i]!=max1){
+            max2=vettore[i];
+        }
+    }
+    printf("i due numeri maggiori sono %d-%d", max1, max2);
+}
