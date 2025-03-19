8f7b9dd40e792ca8ffbb428da7ad2a0e11d0c7c9
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-03-19 13:50:22 +0000

    libArray

--- informatica/Arrei/libArray.c
+++ informatica/Arrei/libArray.c
@@ -1,21 +1,20 @@
-/**
- * funzione che stampa gli elementi di un vettore.
- * @param int [] vettore di interi 
- * @param int dimensione del vettore 
- * @param char separatore tra un elemento e un altro 
- */
-void stampaVettore(int vettore[], int dim, char sep);
-/**
- * funzione che riempie un vettore con valori scelti dall'utente.
- * @param int [] vettore di interi 
- * @param int dimensione del vettore 
- */
-void riempiVettore(int vettore[], int dim);
-/**
- * funzione riempie un vettore con numeri casuali compresi tra un minimo e un massimo.
- * @param int [] vettore di interi 
- * @param int dimensione del vettore 
- * @param int minimo valore minimo dei numeri casuali
- * @param int massimo valore dei numeri casuali
- */
-void riempiVettoreCasuale(int vettore[], int dim, int minimo, int massimo);
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void stampaVettore(int vettore[], int dim, char sep){
+    for(int i=0; i<dim; i++){
+        printf("%d%c", vettore[i], sep);
+    }
+}
+void riempiVettore(int vettore[], int dim){
+    for(int i=0; i<dim; i++){
+        printf("inserici l'elemento in posizione di %d: ", i);
+        scanf("%d", &vettore[i]);
+    }
+}
+void riempiVettoreCasuale(int vettore[], int dim, int minimo, int massimo){
+    srand(time(NULL));
+    for(int i=0; i<dim; i++){
+        vettore[i]=rand()%(massimo-minimo+1)+minimo;
+    }
+}
