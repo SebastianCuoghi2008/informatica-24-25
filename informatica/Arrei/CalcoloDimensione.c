f7b57d83e69b7d56b41a9c7ad32ecff2c0f5f9bd
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-03-26 14:02:25 +0000

    CalcoloDimensione

---
+++ informatica/Arrei/CalcoloDimensione.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "libArray.h"
+#include "libArray.c"
+
+int main(){
+    int v[10] = {0};
+    riempiVettoreCasuale(v, 10, 0, 10);
+    int d = sizeof(v)/sizeof(v[0]);
+    printf("\ndim: %d\n\n\n", d);
+    stampaVettore(v, 10, '\n');
+    return 0;
+}
