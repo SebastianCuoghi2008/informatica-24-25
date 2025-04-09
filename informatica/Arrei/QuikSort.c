a432761ad59aaed087226c69f57d3e034822889e
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-04-09 12:53:46 +0000

    QuikSort

---
+++ informatica/Arrei/QuikSort.c
@@ -0,0 +1,25 @@
+/*scrivi un programma che genera un vettore casuale e lo ordina usando quicksort*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "libArray.c"
+#define DIM 20000
+
+int main(){
+    srand(time(NULL));
+    int vett[DIM];
+    double start, end;
+    double elapsed_time;
+
+    riempiVettoreCasuale(vett, DIM,0,1000);
+    //stampaVettore(vett, DIM, '-');
+    start = (double)clock();
+    quickSort(vett, DIM, 0, DIM-1);
+    end = (double)clock();
+    elapsed_time = ((double)(end - start) );
+    printf("\n\nTempo di esecuzione: %.2f ms\n", elapsed_time);
+    printf("\n\n");
+    //stampaVettore(vett,DIM, '-' );
+    return 0;
+}
