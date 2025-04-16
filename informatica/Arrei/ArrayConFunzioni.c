ee255808706066a6e0f230fba1244e21a9cb65c9
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-04-16 07:18:19 +0000

    ArrayConFunzioni

---
+++ informatica/Arrei/ArrayConFunzioni.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define DIM 5
+
+void stampaVettore(int dim, int v[]);
+void riempiVettore(int dim, int v[]);
+
+int main(){
+    int vettore[DIM];
+
+    riempiVettore(DIM, vettore);
+    stampaVettore(DIM, vettore);
+}
+void riempiVettore(int dim, int v[]){
+    for(int i=0; i<dim; i++){
+        printf("Inserisci il valore per la cella %d: ", i);
+        scanf("%d", &v[i]);
+    }
+}
+void stampaVettore(int dim, int v[]){
+    for(int i = 0; i<dim; i++){
+        printf("%d\t", *(v+i));
+    }
+}
